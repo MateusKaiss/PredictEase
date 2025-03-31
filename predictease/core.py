f031f888dbac78ae749c618b74aed1cdc0dfeb6b
Mateus Kaiss <mateus.kaiss@ufpr.br>
2025-03-31 13:18:19 -0300

    simple models, baseline

--- predictease/core.py
+++ predictease/core.py
@@ -7,12 +7,18 @@ from .analysis.visualization import (
     plot_exogenous_over_time,
 )
 from .models.arima import ARIMAModel
+from .models.baseline import MeanModel, NaiveModel, SeasonalNaiveModel
 from .models.ml_models import MLModel
 from .models.prophet import ProphetModel
 
 
 def run(
-    endog_path, exog_path=None, explore=False, model=None, forecast_steps=10
+    endog_path,
+    exog_path=None,
+    explore=False,
+    model=None,
+    forecast_steps=10,
+    seasonal_length=12,
 ):
     print(f'Loading endogenous data from: {endog_path}')
     data: TimeSeriesDataset = load_data(endog_path, exog_path)
@@ -60,19 +66,34 @@ def run(
         exog['date'] = pd.to_datetime(exog['date'])
         exog.set_index('date', inplace=True)
 
-    X = exog.reindex(y.index)
+        X = exog.reindex(y.index)
 
-    if X.isnull().any().any():
-        print(
-            ' Exogenous data has missing values. Filling them with forward fill.'
-        )
-        X = X.fillna(method='ffill')
+        if X.isnull().any().any():
+            print(
+                ' Exogenous data has missing values. Filling them with forward fill.'
+            )
+            X = X.fillna(method='ffill')
 
         ml_model = MLModel(model_type=model)
         ml_model.fit(X, y)
 
         X_future = exog.iloc[-forecast_steps:]
         forecast = ml_model.predict(X_future)
+
+        print(f'\n Forecast:\n{forecast}')
+
+    elif model in ['naive', 'mean', 'seasonal_naive']:
+        print(f'\n Running baseline model: {model}...')
+
+        if model == 'naive':
+            baseline_model = NaiveModel()
+        elif model == 'mean':
+            baseline_model = MeanModel()
+        elif model == 'seasonal_naive':
+            baseline_model = SeasonalNaiveModel(season_length=seasonal_length)
+
+        baseline_model.fit(y)
+        forecast = baseline_model.predict(steps=forecast_steps)
 
         print(f'\n Forecast:\n{forecast}')
 
